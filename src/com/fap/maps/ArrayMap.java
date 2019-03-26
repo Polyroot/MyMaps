4f803402e931c16a1d566565528c8820e6eae5d2
Polyroot <rezov.aleksandr@mail.ru>
2019-03-26 23:32:15 +0300

    create ArrayMap

--- src/com/fap/maps/ArrayMap.java
+++ src/com/fap/maps/ArrayMap.java
@@ -1,9 +1,38 @@
 package com.fap.maps;
 
-public class ArrayMap {
+public class ArrayMap<K,V> {
 
+    private Object[][] pairs;
+    int index;
 
+    public ArrayMap(int length) {
+        pairs = new Object[length][2];
 
-    int x;
+    }
 
+    public void put(K key, V value){
+        index++;
+        if (index >= pairs.length) throw new ArrayIndexOutOfBoundsException();
+        pairs[index] = new Object[]{key, value};
+    }
+
+    public V get(K key){
+
+        for (int i=0; i < index; i++)
+            if (pairs[i][0] == key)
+                return (V) pairs[i][1];
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0; i<index; i++){
+            stringBuilder.append(pairs[i][0]);
+            stringBuilder.append(" : ");
+            stringBuilder.append(pairs[i][1]);
+            if (i<index-1) stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
 }
